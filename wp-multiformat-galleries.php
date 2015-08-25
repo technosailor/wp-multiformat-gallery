3d824b4d428f438f18bb2c5b931069b880357fb2
Aaron Brazell <aaron@technosailor.com>
2015-08-25 20:14:39 +0000

    Init

---
+++ wp-multiformat-galleries.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: WP Multiformat Galleries
+Plugin URI:  https://github.com/technosailor/wp-multiformat-gallery
+Description: Renders WordPress galleries in a variety of ways
+Version:     0.1-alpha
+Author:      Aaron Brazell
+Author URI:  http://technosailor.com
+Text Domain: wp-multiformat-galleries
+Domain Path: /langs
+*/
+
+define( 'WPMFG_VERSION', '0.1-alpha' );
+define( 'WPMFG_URL', plugin_dir_url( __FILE__ ) );
+define( 'WPMFG_PATH', dirname( __FILE__ ) . '/' );
+define( 'WPMFG_BASENAME', plugin_basename( __FILE__ ) );
+define( 'WPMFG_CLASS_DIR', WPMFG_PATH . 'classes/' );
